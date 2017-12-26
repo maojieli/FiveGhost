cad12b625128cfa3212537ec1bd934d92e424168
asus <1872673159@qq.com>
2017-12-26 09:36:26 +0800

    view的所有

--- app/src/main/java/com/admiralfivetigers/fiveghost/MainActivity.java
+++ app/src/main/java/com/admiralfivetigers/fiveghost/MainActivity.java
@@ -1,31 +1,98 @@
 package com.admiralfivetigers.fiveghost;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.admiralfivetigers.fiveghost.widget.customtitle.TitleBarView;
+import com.admiralfivetigers.fiveghost.widget.progressbar.NumberProgressBar;
+import com.admiralfivetigers.fiveghost.widget.progressbar.OnProgressBarListener;
+import com.admiralfivetigers.fiveghost.widget.pulldowntorefresh.QQRefreshHeader;
+import com.admiralfivetigers.fiveghost.widget.pulldowntorefresh.RefreshLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-
-    private TitleBarView tb;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        tb.setLeftImageisible(View.GONE);
-        tb.setRightImageisible(View.GONE);
-        tb.setLeftTextisible(View.VISIBLE);
-        tb.setRightTextisible(View.VISIBLE);
+        setContentView(R.layout.activity_main);
 
-        tb.setLeftTextView("左侧文本");
-        tb.setRightTextView("右侧文本");
-        tb.setTitleTextView("我是标题");
+        listView = (ListView) findViewById(R.id.listView);
+
+        final RefreshLayout refreshLayout = (RefreshLayout) findViewById(R.id.refreshLayout);
+        if (refreshLayout != null) {
+            // 刷新状态的回调
+            refreshLayout.setRefreshListener(new RefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    // 延迟3秒后刷新成功
+                    refreshLayout.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            refreshLayout.refreshComplete();
+                            if (listView != null) {
+                                listView.setAdapter(new MainAdapter());
+                            }
+                        }
+                    }, 3000);
+                }
+            });
+        }
+
+        QQRefreshHeader header  = new QQRefreshHeader(this);
+        refreshLayout.setRefreshHeader(header);
+        refreshLayout.autoRefresh();
+
 
     }
 
-}
+    class MainAdapter extends BaseAdapter {
 
+        @Override
+        public int getCount() {
+            return 50;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null) {
+                TextView textView =  new TextView(MainActivity.this);
+                textView.setText(String.valueOf(position));
+                textView.setTextColor(Color.BLACK);
+                textView.setBackgroundColor(0x55ff0000);
+                textView.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,200));
+                textView.setGravity(Gravity.CENTER);
+                convertView = textView;
+            } else {
+                ((TextView) convertView).setText(String.valueOf(position));
+            }
+
+            return convertView;
+        }
+    }
+}
---
+++ app/src/main/java/com/admiralfivetigers/fiveghost/widget/pulldowntorefresh/State.java
@@ -0,0 +1,9 @@
+package com.admiralfivetigers.fiveghost.widget.pulldowntorefresh;
+
+/**
+ * 认真的人到最后都是难过 2017/12/25.
+ */
+
+public enum  State {
+    RESET, PULL, LOADING, COMPLETE
+}

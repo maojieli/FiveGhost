163c979ea98df59636b60fc45020943ec1d9ec34
韩学文 <799767515@qq.com>
2017-12-26 13:29:32 +0800

    韓叁叁

--- app/src/main/java/com/admiralfivetigers/fiveghost/MainActivity.java
+++ app/src/main/java/com/admiralfivetigers/fiveghost/MainActivity.java
@@ -1,98 +1,37 @@
 package com.admiralfivetigers.fiveghost;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.admiralfivetigers.fiveghost.widget.progressbar.NumberProgressBar;
-import com.admiralfivetigers.fiveghost.widget.progressbar.OnProgressBarListener;
-import com.admiralfivetigers.fiveghost.widget.pulldowntorefresh.QQRefreshHeader;
-import com.admiralfivetigers.fiveghost.widget.pulldowntorefresh.RefreshLayout;
-
-import java.util.Timer;
-import java.util.TimerTask;
+import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ListView listView;
+
+    private Button bu_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        listView = (ListView) findViewById(R.id.listView);
-
-        final RefreshLayout refreshLayout = (RefreshLayout) findViewById(R.id.refreshLayout);
-        if (refreshLayout != null) {
-            // 刷新状态的回调
-            refreshLayout.setRefreshListener(new RefreshLayout.OnRefreshListener() {
-                @Override
-                public void onRefresh() {
-                    // 延迟3秒后刷新成功
-                    refreshLayout.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            refreshLayout.refreshComplete();
-                            if (listView != null) {
-                                listView.setAdapter(new MainAdapter());
-                            }
-                        }
-                    }, 3000);
-                }
-            });
-        }
-
-        QQRefreshHeader header  = new QQRefreshHeader(this);
-        refreshLayout.setRefreshHeader(header);
-        refreshLayout.autoRefresh();
-
+        initView();
 
     }
 
-    class MainAdapter extends BaseAdapter {
+    private void initView() {
+        bu_main = (Button) findViewById(R.id.bu_main);
 
-        @Override
-        public int getCount() {
-            return 50;
-        }
+        bu_main.setOnClickListener(this);
+    }
 
-        @Override
-        public Object getItem(int position) {
-            return position;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if(convertView == null) {
-                TextView textView =  new TextView(MainActivity.this);
-                textView.setText(String.valueOf(position));
-                textView.setTextColor(Color.BLACK);
-                textView.setBackgroundColor(0x55ff0000);
-                textView.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,200));
-                textView.setGravity(Gravity.CENTER);
-                convertView = textView;
-            } else {
-                ((TextView) convertView).setText(String.valueOf(position));
-            }
-
-            return convertView;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bu_main:
+                startActivity(new Intent(this,ZxingActivity.class));
+                break;
         }
     }
 }
